3c36da6e38ec5537294fbaceb09f98b2d0133c4f
tomashined <45771896+tomashined@users.noreply.github.com>
2021-01-27 14:59:45 +0300

    Lights

---
+++ Assets/TrafficLight/Scripts/Light.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] private Image colorLight;
+    [SerializeField] private Color color;
+    [SerializeField] private int activeTime;
+
+    public Image ColorLight { get => (colorLight == null) ? GetComponent<Image>() : colorLight; set => colorLight = value; }
+    public int ActiveTime { get => (activeTime == 0) ? 3 : activeTime; set => activeTime = value; }
+
+    void Start()
+    {
+        ActivateLight(false);
+    }
+
+    public void ActivateLight(bool activate = true)
+    {
+        if (activate)
+        {
+            ColorLight.color = color;
+            StartCoroutine(Glow());
+        }
+        else
+        {
+            ColorLight.color = Color.white;
+        }
+    }
+    private IEnumerator Glow()
+    {
+        yield return new WaitForSeconds(ActiveTime);
+        ActivateLight(false);
+    }
+}
---
+++ Assets/TrafficLight/Scripts/TrafficLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    [SerializeField] private List<Light> Lights;
+
+    void Start()
+    {
+        if (Lights == null) return;
+        StartCoroutine(Light());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Light()
+    {
+        foreach (Light light in Lights)
+        {
+            light.ActivateLight();
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
